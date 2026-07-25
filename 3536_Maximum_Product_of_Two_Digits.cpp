feb0a8d87f9de0426f0de1792defcbac6975a623
deepakdevfile <deepakdevfile@gmail.com>
2026-07-25 11:25:48 +0530

    0001 25 july 2026 leetcode problem of the day

---
+++ 3536_Maximum_Product_of_Two_Digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int m1 = max(n % 10, (n % 100) / 10);
+        int m2 = min(n % 10, (n % 100) / 10);
+        int temp;
+        n = n / 100;
+        while(n > 0){
+            temp = n % 10;
+            if(temp >= m1){
+                m2 = m1;
+                m1 = temp;
+            } else if(temp >= m2){
+                m2 = temp;
+            }
+            n = n / 10;
+        }
+        return m1*m2;
+    }
+};
